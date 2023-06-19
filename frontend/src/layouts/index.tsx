04d04b282ee1c04fc6b40b5b94e12dcd4e3b1d3a
Blockchain-Key <1416893908@qq.com>
2023-06-19 16:03:58 +0800

    更新layouts

--- frontend/src/layouts/index.tsx
+++ frontend/src/layouts/index.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'umi'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-
+// border-red-500 border border-solid
 export default function Layout() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#F3E3FA] to-[#D8ACEC]">
       <div className="container mx-auto flex flex-col justify-between min-h-screen">
         <Header></Header>
-        <main className="flex-grow border-red-500 border border-solid">
+        <main className="flex-grow">
           <Outlet />
         </main>
         <Footer></Footer>
